ee16b6c51ff7369635baddcd34480efe0e137c8e
Youssef Ashraf <youssefashrafeissa@gmail.com>
2024-05-28 14:44:59 +0300

    refactor: fixed CORS error

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  console.log("Hello");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST , PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
