5fd4d3f610d15f430a4bd479677808612a1334f1
Youssef Ashraf <youssefashrafeissa@gmail.com>
2024-06-03 19:53:05 +0300

    feat: pagination done

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,9 +4,19 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  const page = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((page - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
-      res.status(200).json({ posts: posts });
+      res.status(200).json({ posts: posts, totalItems: totalItems });
     })
     .catch((err) => next(err));
 };
